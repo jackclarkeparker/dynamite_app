757e513b7a507769db2da9e9595256aaee2fe0b2
Jack <jclarkeparker@gmail.com>
2023-07-10 14:31:54 +1200

    Venues Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_020429) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_022115) do
   create_table "tutors", force: :cascade do |t|
     t.string "preferred_name"
     t.string "first_name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_020429) do
     t.string "email_address"
     t.string "phone_number"
     t.string "delivery_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
