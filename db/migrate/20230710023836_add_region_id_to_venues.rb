b88a8097ef7e982b9fa717a3b3292b126b29c16f
Jack <jclarkeparker@gmail.com>
2023-07-11 15:42:53 +1200

    Added reversibility to venues.region_id adding migration

--- db/migrate/20230710023836_add_region_id_to_venues.rb
+++ db/migrate/20230710023836_add_region_id_to_venues.rb
@@ -1,5 +1,9 @@
 class AddRegionIdToVenues < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :venues, :region_id, :integer
+  end
+
+  def down
+    drop_column :venues, :region_id
   end
 end
