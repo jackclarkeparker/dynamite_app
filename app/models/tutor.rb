c9d6bbe7d74880476a714701ea7baca5e05657be
Jack <jclarkeparker@gmail.com>
2023-07-13 11:29:45 +1200

    Tutor and Venue validations

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,8 @@
 class Tutor < ApplicationRecord
   has_many :lessons
+
+  validates :preferred_name, :first_name, :last_name,
+            :email_address, :phone_number, :delivery_address,
+            presence: true
+  validates :email_address, :phone_number, :full_name, uniqueness: true
 end
