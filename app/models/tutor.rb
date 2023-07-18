e9e6472774f57e1dd6d795c22730476714a3e002
Jack CP <jclarkeparker@gmail.com>
2023-07-18 14:25:47 +1200

    Tutor validations updated
    - No two active tutors with the same email or phone number
    - Preferred_name not required by validation

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,10 +1,28 @@
+class UniqueTutorPhoneAndEmailValidator < ActiveModel::Validator
+  def validate(record)
+    other_active_tutors = Tutor.all.select do |t|
+      t.valid_until == ApplicationRecord::FUTURE_EPOCH
+    end.reject { |t| t.entity_id == record.entity_id }
+
+    if other_active_tutors.any? { |t| t.email_address == record.email_address }
+      record.errors.add :base, "Email address in use by another tutor"
+    end
+
+    if other_active_tutors.any? { |t| t.phone_number == record.phone_number }
+      record.errors.add :base, "Phone number in use by another tutor"
+    end
+  end
+end
+
 class Tutor < ApplicationRecord
   has_many :lessons
 
-  validates :preferred_name, :first_name, :last_name,
-            :email_address, :phone_number, :delivery_address,
-            presence: true
-  validates :email_address, :phone_number, :full_name, uniqueness: true
+  validates :first_name, :last_name, :email_address,
+            :phone_number, :delivery_address,
+            :entity_id, presence: true
+  
+  # No two active tutors with same email address, or phone number.
+  validates_with UniqueTutorPhoneAndEmailValidator
 
   before_create :set_full_name_and_valid_until
 
@@ -15,6 +33,14 @@ class Tutor < ApplicationRecord
     self.email_address == other_tutor.email_address &&
     self.phone_number == other_tutor.phone_number &&
     self.delivery_address == other_tutor.delivery_address
+  end
+
+  def to_s
+    if self.preferred_name && self.preferred_name.length > 0
+      self.preferred_name
+    else
+      self.first_name
+    end
   end
 
   private
