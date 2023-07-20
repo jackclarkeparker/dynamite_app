5e9e7c11321bd8d244ec11a7b2eabcc8e25462e9
Jack CP <jclarkeparker@gmail.com>
2023-07-21 09:56:52 +1200

    students/_student displays region name

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,7 +6,7 @@ class StudentsController < ApplicationController
 
   # GET /students or /students.json
   def index
-    @students = Student.all
+    @students = Student.includes(:region) #, :school) For when we add the schools table, and associate with students
   end
 
   # GET /students/1 or /students/1.json
