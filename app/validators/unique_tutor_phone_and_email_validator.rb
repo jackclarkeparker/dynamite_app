a8b3fc9b60086b7992fd4d02d610cd2c34ab5dca
Jack CP <jclarkeparker@gmail.com>
2023-07-22 11:10:38 +1200

    Shifted tutors validator into app/validators

---
+++ app/validators/unique_tutor_phone_and_email_validator.rb
@@ -0,0 +1,15 @@
+class UniqueTutorPhoneAndEmailValidator < ActiveModel::Validator
+  def validate(record)
+    other_active_tutors = Tutor.all.select do |t|
+      t.valid_until == ApplicationRecord::FUTURE_EPOCH
+    end.reject { |t| t.entity_id == record.entity_id }
+
+    if other_active_tutors.any? { |t| t.email_address == record.email_address }
+      record.errors.add :base, "Email address in use by another tutor"
+    end
+
+    if other_active_tutors.any? { |t| t.phone_number == record.phone_number }
+      record.errors.add :base, "Phone number in use by another tutor"
+    end
+  end
+end
