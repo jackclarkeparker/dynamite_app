6cf00ac5cdd0a9d91ad89ec2201756a3c9b3c0bb
Jack CP <jclarkeparker@gmail.com>
2023-07-24 10:24:56 +1200

    Lesson helpers, and view updates
    - time display formatting, correct default values for select inputs
    - _form updated to use default values provided by helpers
    - _lesson displays additional attributes
    - display for time in booker view tweaked

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,4 +1,27 @@
 module LessonsHelper
+  def default_start_time
+    params[:lesson] && params[:lesson][:start_time] ||
+    @lesson && @lesson.start_time && @lesson.start_time.strftime("%H:%M:%S")
+  end
+
+  def default_capacity
+    params[:lesson] && params[:lesson][:capacity] ||
+    @lesson && @lesson.capacity || 4
+  end
+
+  def default_duration
+    params[:lesson] && params[:lesson][:duration] ||
+    @lesson && @lesson.duration || 30
+  end
+
+  def display_lesson_time_range(lesson)
+    display_start_time(lesson) + ' - ' + display_end_time(lesson)
+  end
+
+  def display_end_time(lesson)
+    lesson.end_time.strftime("%-l:%M%P")
+  end
+
   def display_start_time(lesson)
     lesson.start_time.strftime("%-l:%M%P")
   end
