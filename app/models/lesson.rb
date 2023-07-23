f3a896fd05b6c0ed982475a8111b7118879f80e9
Jack CP <jclarkeparker@gmail.com>
2023-07-24 10:34:07 +1200

    Lesson validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,8 +6,18 @@ class Lesson < ApplicationRecord
 
   after_initialize :set_end_time
 
+  validates :standard_price, :capacity, :duration, :start_time, presence: true
   validate :validate_venue_id
   validate :validate_tutor_id
+  validate :validate_day
+  validates :standard_price, numericality: {
+    in: 1..50, message: "must be no less tha 1, and no greater than 50"
+  }, allow_blank: true
+  validates :capacity, numericality: {
+    in: 1..6, message: "can't be greater than 6"
+  }, allow_blank: true
+  validate :validate_tutor_clash
+  validate :validate_venue_clash
 
   def day
     Date::DAYNAMES[self.week_day_index]
@@ -15,10 +25,46 @@ class Lesson < ApplicationRecord
 
   private
 
+    def validate_day
+      unless week_day_index && week_day_index.between?(0, 6)
+        errors.add(:day, "must be selected")
+      end
+    end
+
     def set_end_time
       if start_time && duration
         seconds_per_minute = 60
         self.end_time = start_time + (duration * seconds_per_minute)
+      end
+    end
+
+    def validate_tutor_clash
+      lessons_to_check = Lesson.where(
+        tutor_id: tutor_id, week_day_index: week_day_index
+      ).reject { |lesson| lesson.id == self.id }
+
+      is_a_clash = lessons_to_check.any? do |existing|
+        (existing.start_time...existing.end_time).cover?(self.start_time) ||
+        ((existing.start_time + 1)..existing.end_time).cover?(self.end_time)
+      end
+
+      if is_a_clash
+        errors.add(:tutor, "is already teaching during this timeslot")
+      end
+    end
+
+    def validate_venue_clash
+      lessons_to_check = Lesson.where(
+        venue_id: venue_id, week_day_index: week_day_index
+      ).reject { |lesson| lesson.id == self.id }
+
+      is_a_clash = lessons_to_check.any? do |existing|
+        (existing.start_time...existing.end_time).cover?(self.start_time) ||
+        ((existing.start_time + 1)..existing.end_time).cover?(self.end_time)
+      end
+
+      if is_a_clash
+        errors.add(:venue, "is already being used during this timeslot")
       end
     end
 end
