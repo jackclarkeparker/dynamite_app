e86e4fe48aa2c4bc8830f07f413239ce40d707ca
Jack CP <jclarkeparker@gmail.com>
2023-07-23 13:24:42 +1200

    tutor region existence validation
    - This is implemented as a private method in the tutor model
    - Also wrote tutor model and controller tests to monitor expected
      behaviour for when an invalid region_id is supplied by a client

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -8,6 +8,7 @@ class Tutor < ApplicationRecord
             :phone_number, :delivery_address,
             :entity_id, presence: true
   validates :region_id, presence: { message: "must be selected" }
+  validate :validate_region_id
   validates :phone_number, format: {
     with: /\s/,
     message: 'suffix must be between six and nine digits long'
@@ -39,5 +40,11 @@ class Tutor < ApplicationRecord
     def set_full_name_and_valid_until
       self.full_name = "#{self.first_name} #{self.last_name}"
       self.valid_until = ApplicationRecord::FUTURE_EPOCH
+    end
+
+    def validate_region_id
+      if region_id && !Region.exists?(id: region_id)
+        errors.add(:region, 'must be a valid selection')
+      end
     end
 end
