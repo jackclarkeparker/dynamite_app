8347da1f6d66a6120f6d530cefa70a92c8444760
Jack CP <jclarkeparker@gmail.com>
2023-07-27 11:59:22 +1200

    schema conflict addressed

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_24_093550) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "lessons", force: :cascade do |t|
     t.integer "tutor_id", null: false
     t.integer "venue_id", null: false
