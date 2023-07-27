621eb87bf07666135606214e185c7ff0b8e77243
Jack CP <jclarkeparker@gmail.com>
2023-07-28 10:32:29 +1200

    More venue validations
    - Standard price must be present
    - Address uniqueness message added
    - Name uniqueness now checked with helper validate_name_unique_to_region
    - Private #validate_name_unique_to_region implemented

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,15 +4,24 @@ class Venue < ApplicationRecord
   belongs_to :region, optional: true
   has_many :lessons
 
-  validates :name, :address, presence: true
-  validates :name, :address, uniqueness: true
-
+  validates :name, :address, :standard_price, presence: true
+  validates :address, uniqueness: { message: 'in use by another venue' }
   validate :validate_region_id
+  validate :validate_name_unique_to_region
 
   before_destroy :ensure_no_active_lessons
 
 
   private
+
+    def validate_name_unique_to_region
+      if Region.exists?(id: region_id)
+        region = Region.find(region_id)
+        if region.venues.any? { |v| v.name == name }
+          errors.add(:name, "already used by another venue in this region")
+        end
+      end
+    end
 
     def ensure_no_active_lessons
       if lessons.any?
