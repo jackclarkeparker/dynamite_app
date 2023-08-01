333639023f8bc21834dffda8b15aaa8182bc0ff4
Jack CP <jclarkeparker@gmail.com>
2023-08-01 16:06:10 +1200

    [REFAC] StudentsHelper#primary_contact_field
    Extracted logic for generating a link and styled span to the /students
    view into the helper method.

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,7 +2,19 @@ module StudentsHelper
   def try_display_primary_contact_relation(student)
     relation = student.primary_contact_relation
     if relation && relation.length > 0
-      ' (' + student.primary_contact_relation + ')' 
+      ' (' + student.primary_contact_relation + ')'
+    end
+  end
+
+  def primary_contact_field(student)
+    if student.primary_contact
+      link_to(student.primary_contact) + content_tag(
+        :span,
+        try_display_primary_contact_relation(student),
+        class: "primary-contact-relation"
+      )
+    else
+      'N/A'
     end
   end
 end
