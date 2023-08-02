4dc54368def480bef133caeb36f96fd4bc1546aa
Jack CP <jclarkeparker@gmail.com>
2023-08-02 19:15:34 +1200

    [CHG] Move existing actions+views to StudentContact
    Shifting everything setup previously in:
    - app/views/students into app/views/student_contacts
    - app/controllers/students_controller into
      app/controllers/student_contacts_controller

--- app/controllers/student_contacts_controller.rb
+++ app/controllers/student_contacts_controller.rb
@@ -1,5 +1,9 @@
 class StudentContactsController < ApplicationController
+  before_action :set_student_with_student_contacts, only: %i[ new_contact assign_contact ]
+
+  # GET /students/1/new_contact
   def new_contact
+    @student_contact = StudentContact.new(student_id: @student.id)
   end
 
   def new_student
@@ -10,4 +14,29 @@ class StudentContactsController < ApplicationController
 
   def edit_student
   end
+
+  # POST /students/1/assign_contact
+  def assign_contact
+    @student_contact = StudentContact.new(student_contact_params)
+
+    respond_to do |format|
+      if @student_contact.save
+        format.html { redirect_to student_url(@student), notice: "Contact was successfully added." }
+        format.json { render :show, status: :created, location: @student }
+      else
+        format.html { render :new_contact, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def set_student_with_student_contacts
+      @student = Student.includes(:student_contacts).find(params[:id])
+    end
+
+    def student_contact_params
+      params.require(:student_contact).permit(:student_id, :contact_id, :contact_relation, :primary_contact, :account_holder)
+    end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[ show edit update destroy ]
-  before_action :set_student_with_student_contacts, only: %i[ new_contact assign_contact ]
 
   # GET /students or /students.json
   def index
@@ -75,42 +74,14 @@ class StudentsController < ApplicationController
     end
   end
 
-  # GET /students/1/new_contact
-  def new_contact
-    @student_contact = StudentContact.new(student_id: @student.id)
-  end
-
-  # POST /students/1/assign_contact
-  def assign_contact
-    @student_contact = StudentContact.new(student_contact_params)
-
-    respond_to do |format|
-      if @student_contact.save
-        format.html { redirect_to student_url(@student), notice: "Contact was successfully added." }
-        format.json { render :show, status: :created, location: @student }
-      else
-        format.html { render :new_contact, status: :unprocessable_entity }
-        format.json { render json: @student.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
     end
 
-    def set_student_with_student_contacts
-      @student = Student.includes(:student_contacts).find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def student_params
       params.require(:student).permit(:preferred_name, :first_name, :last_name, :birthday, :year_group, :gender, :region_id, :keyboard, :age)
-    end
-
-    def student_contact_params
-      params.require(:student_contact).permit(:student_id, :contact_id, :contact_relation, :primary_contact, :account_holder)
     end
 end
