ec4a1b87f8f04c9af5a1b02bcad44ebda8f4f0d3
Jack CP <jclarkeparker@gmail.com>
2023-08-06 11:34:52 +1200

    [BUILD] LessonMembersController actions
    \#new, #create, and #destroy actions have been written.
    
    Mixing in the SlowlyChangingDimensionsHelper to decomission old
    lesson_member instances rather than delete them when #destroy is
    invoked (The history of "what students attended which classes" seems
    relevant to hold onto)

--- app/controllers/lesson_members_controller.rb
+++ app/controllers/lesson_members_controller.rb
@@ -1,10 +1,47 @@
 class LessonMembersController < ApplicationController
+  include SlowlyChangingDimensionHelpers
+
   def new_lesson_member
-    # respond_to do |format|
-    #   format.html { render partial: :new_lesson_member }
-    # end
+    @lesson_member = LessonMember.new
+    @lesson_id = params[:lesson_id]
+
+    respond_to do |format|
+      format.html { render partial: 'new_lesson_member' }
+    end
+  end
+
+  def create_lesson_member
+    @lesson_member = LessonMember.new(lesson_member_params)
+    @lesson_id = params[:lesson_id]
+
+    respond_to do |format|
+      if @lesson_member.save
+        format.html { redirect_to lesson_url(@lesson_id), notice: "Student added to lesson." }
+        format.json { render :show, status: :created, location: @lesson_member }
+      else
+        format.html { render partial: 'new_lesson_member', status: :unprocessable_entity }
+        format.json { render json: @lesson_member.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy_lesson_member
+    @lesson_member = LessonMember.find_by({
+      lesson_id: params[:lesson_id],
+      student_id: params[:student_id],
+      valid_until: ApplicationRecord::FUTURE_EPOCH,
+    })
+    decomission_old_version(@lesson_member, decomission_timestamp: Time.now)
+
+    respond_to do |format|
+      format.html { redirect_to lesson_url(@lesson_member.lesson_id), notice: "Student removed from lesson." }
+      format.json { head :no_content }
+    end
   end
+
+  private
+
+    def lesson_member_params
+      params.require(:lesson_member).permit(:lesson_id, :student_id)
+    end
 end
