e081eda9924bf6f7d3a6f4ee18ac85e0dc56cdc8
Jack CP <jclarkeparker@gmail.com>
2023-08-07 11:29:20 +1200

    [FEAT] LessonMember validations
    I might add to these, specifically in the event that LessonMembers
    become add-able from the students/show template, we'll need to
    validate the lesson selection as well.

--- app/models/lesson_member.rb
+++ app/models/lesson_member.rb
@@ -1,6 +1,10 @@
 class LessonMember < ApplicationRecord
+  include RelatedModelValidator
+
   belongs_to :lesson
-  belongs_to :student
+  belongs_to :student, optional: true
 
   default_scope { where(valid_until: FUTURE_EPOCH) }
+
+  validate :validate_student_id
 end
