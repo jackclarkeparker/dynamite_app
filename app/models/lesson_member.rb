b61be45a4a88ad1cc7d65ad5214c41701c147976
Jack CP <jclarkeparker@gmail.com>
2023-08-06 12:54:41 +1200

    [FEAT] set_valid_until hook for LessonMember callback
    So that newly created LessonMembers have their 'valid_until' attribute
    set to FUTURE_EPOCH.

--- app/models/lesson_member.rb
+++ app/models/lesson_member.rb
@@ -1,4 +1,12 @@
 class LessonMember < ApplicationRecord
   belongs_to :lesson
   belongs_to :student
+
+  before_create :set_valid_until
+
+  private
+
+    def set_valid_until
+      self.valid_until = FUTURE_EPOCH
+    end
 end
