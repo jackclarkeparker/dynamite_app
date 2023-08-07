b5369ea8d24f3ffeb607c5c7d94894fdaac1afd6
Jack CP <jclarkeparker@gmail.com>
2023-08-07 12:57:45 +1200

    [CHG] Rename routes to conform with standard names
    - POST create_lesson_member   -> becomes -> POST lesson_members
    - PATCH update_lesson_member  -> becomes -> PATCH lesson_member
    - DELETE update_lesson_member -> becomes -> DELETE lesson_member
    
    And so on for contact_relationships and student_relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   # LessonMembers routes
   get 'lessons/:lesson_id/lesson_members/new', to: 'lesson_members#new_lesson_member', as: :new_lesson_member
-  post 'lessons/:lesson_id/lesson_members', to: 'lesson_members#create_lesson_member', as: :create_lesson_member
+  post 'lessons/:lesson_id/lesson_members', to: 'lesson_members#create_lesson_member', as: :lesson_members
   delete 'lessons/:lesson_id/lesson_members/:student_id', to: 'lesson_members#destroy_lesson_member', as: :lesson_member
 
   # StudentContacts routes
   get '/students/:student_id/contacts/new', to: 'student_contacts#new_contact_relationship', as: :new_contact_relationship
-  post '/students/:student_id/contacts', to: 'student_contacts#create_contact_relationship', as: :create_contact_relationship
+  post '/students/:student_id/contacts', to: 'student_contacts#create_contact_relationship', as: :contact_relationships
 
   get '/students/:student_id/contacts/:contact_id/edit', to: 'student_contacts#edit_contact_relationship', as: :edit_contact_relationship
-  patch '/students/:student_id/contacts/:contact_id', to: 'student_contacts#update_contact_relationship', as: :update_contact_relationship
+  patch '/students/:student_id/contacts/:contact_id', to: 'student_contacts#update_contact_relationship', as: :contact_relationship
 
   delete '/students/:student_id/contacts/:contact_id', to: 'student_contacts#destroy_contact_relationship'
 
   get '/contacts/:contact_id/students/new', to: 'student_contacts#new_student_relationship', as: :new_student_relationship
-  post '/contacts/:contact_id/students', to: 'student_contacts#create_student_relationship', as: :create_student_relationship
+  post '/contacts/:contact_id/students', to: 'student_contacts#create_student_relationship', as: :student_relationships
 
   get '/contacts/:contact_id/students/:student_id/edit', to: 'student_contacts#edit_student_relationship', as: :edit_student_relationship
-  patch '/contacts/:contact_id/students/:student_id', to: 'student_contacts#update_student_relationship', as: :update_student_relationship
+  patch '/contacts/:contact_id/students/:student_id', to: 'student_contacts#update_student_relationship', as: :student_relationship
 
   resources :contacts
   resources :students
