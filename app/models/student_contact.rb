f9f45c86604eb4fa2ab491a812ee13e9bc792d3a
Jack CP <jclarkeparker@gmail.com>
2023-08-09 12:39:33 +1200

    [BUILD] StudentContact#validate_new_combination
    Validation to ensure the same student and contact can't have more than
    one relationship represented in the student_contacts table.

--- app/models/student_contact.rb
+++ app/models/student_contact.rb
@@ -6,6 +6,7 @@ class StudentContact < ApplicationRecord
 
   validate :validate_student_id
   validate :validate_contact_id
+  validate :validate_new_combination
   validates :primary_contact, inclusion: {
     in: [true, false],
     message: "must be selected"
@@ -18,4 +19,22 @@ class StudentContact < ApplicationRecord
     primary_contact == other_student_contact.primary_contact &&
     account_holder == other_student_contact.account_holder
   end
+
+  private
+
+    def validate_new_combination
+      existing_student_contacts = StudentContact.all.reject do |student_contact|
+        student_contact.id == self.id
+      end
+
+      combo_in_use = existing_student_contacts.any? do |student_contact|
+        student_contact.student_id == self.student_id &&
+        student_contact.contact_id == self.contact_id
+      end
+
+      if combo_in_use
+        errors.add(:student, 'must be selected')
+        errors.add(:contact, 'must be selected')
+      end
+    end
 end
