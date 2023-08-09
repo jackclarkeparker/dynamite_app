8f3867ccc728e02d2d8dcfad20f870aa7a33ebbd
Jack CP <jclarkeparker@gmail.com>
2023-08-09 13:00:01 +1200

    [CHG] #decomission_old_version skips validation
    Previously, a newly saved version of a contact would prevent the old
    version from having it's valid_until attribute updated due to a
    validation clash with the new version; i.e. the old tutor's phone and
    email would not register as unique since the new version saved in the
    database shared the same email / phone.
    
    Skipping validation with the #update_attribute method.

--- app/controllers/concerns/slowly_changing_dimension_helpers.rb
+++ app/controllers/concerns/slowly_changing_dimension_helpers.rb
@@ -13,7 +13,6 @@ module SlowlyChangingDimensionHelpers
 =end
 
     def decomission_old_version(old_version, decomission_timestamp:)
-      old_version.valid_until = decomission_timestamp
-      old_version.save
+      old_version.update_attribute(:valid_until, decomission_timestamp)
     end
 end
