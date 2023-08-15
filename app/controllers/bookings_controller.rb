2777220b7b43df7ef084c6804d4eebe9213973f6
Jack CP <jclarkeparker@gmail.com>
2023-08-15 15:10:32 +1200

    [BUILD] Bookings spike - #create action and helpers
    Quite a few helpers here and there. In this spike I was mapping the
    form inputs to the attributes of a Student and Contact model manually,
    using class methods defined on Booking. Also ascertaining the
    lesson_id from the params, and the region_id from a join query on the
    venue of the lesson with that id.
    
    I'm going to go for the Rails convention after this. Will involve a
    refactor, using accepts_nested_attributes_for declarations in the
    Booking model, as well as some fields_for helpers in bookings/_form
    to specify which inputs are for which derivate model instances
    (contact or student)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,55 @@
 class BookingsController < ApplicationController
   include LessonQueryHelper
+  before_action :set_lesson_and_region_ids, only: %i[ new_booking create_booking ]
 
   def lessons
     @lesson_schedule = lessons_by_venue_by_day
   end
 
   def new_booking
+    @booking = Booking.new
   end
+
+  def create_booking
+    contact = Booking.contact_from_booking_params(booking_params)
+    student = Booking.student_from_booking_params(booking_params)
+
+    # contact.insert!
+    # student.insert!
+
+    # LessonMember.create(lesson_id: @lesson_id, student_id: student.id)
+  end
+
+  # def create
+  #   @lesson = Lesson.new(lesson_params)
+
+  #   respond_to do |format|
+  #     if @lesson.save
+  #       format.html { redirect_to lesson_url(@lesson), notice: "Lesson was successfully created." }
+  #       format.json { render :show, status: :created, location: @lesson }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @lesson.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   def new_waiting_list_entry
   end
+
+  private
+
+    def region_id_from_lesson_id(region_id)
+      Lesson.joins(:venue).where("lessons.id" => region_id)
+                          .pluck(:region_id).first
+    end
+
+    def set_lesson_and_region_ids
+      @lesson_id = params[:lesson_id]
+      @region_id = region_id_from_lesson_id(@lesson_id)
+    end
+
+    def booking_params
+      params.require(:booking).permit(:student_name, :student_age, :contact_name, :contact_email, :contact_phone, :additional_details, :lesson_id, :region_id)
+    end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,50 @@
 class Booking < ApplicationRecord
   belongs_to :lesson
   belongs_to :contact
+
+  def self.contact_from_booking_params(booking_params)
+    region_id = booking_params[:region_id]
+    contact_params = booking_params.select do |param, _|
+      param.start_with?('contact')
+    end
+
+    first, last = first_last_from_name(contact_params[:contact_name])
+
+    Contact.new(
+      first_name: first,
+      last_name: last,
+      full_name: contact_params[:contact_name],
+      region_id: region_id,
+      email_address: contact_params[:contact_email],
+      phone_number: contact_params[:phone_number],
+    )
+  end
+
+  def self.student_from_booking_params(booking_params)
+    region_id = booking_params[:region_id]
+    student_params = booking_params.select do |param, _|
+      param.start_with?('student')
+    end
+    first, last = first_last_from_name(student_params[:student_name])
+
+    Student.new(
+      region_id: region_id,
+      first_name: first,
+      last_name: last,
+      full_name: student_params[:student_name],
+      age: student_params[:student_age],
+    )
+  end
+
+  private
+
+    def self.first_last_from_name(name)
+      name_components = name.split(' ')
+
+      if name_components.length > 1
+        [ name_components[0], name_components[-1] ]
+      else
+        [ name_components[0], nil ]
+      end
+    end
 end
