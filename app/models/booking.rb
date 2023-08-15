5e322ac49c5465bbc0dc393ede972329f8b8bc2c
Jack CP <jclarkeparker@gmail.com>
2023-08-15 19:31:04 +1200

    [BUILD] GPT-inspired nested_attributes_for spike
    I've used accepts_nested_attributes_for :student, :contact, and
    currently it raises an ArgumentError with the following message:
    
    No association found for name `student'. Has it been defined yet?
    
    I believe I'll need to define the association to students, which I'm
    going to do using a join table because I ultimately will want a 1:M
    relationship between a booking and the students associated.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,39 @@ class Booking < ApplicationRecord
   belongs_to :lesson
   belongs_to :contact
 
+  # has_many :students, through: :booking_students
+
+  # I think we'll want to set this to :students instead of :student
+  # once the booking form allows more than one student to be booked.
+  accepts_nested_attributes_for :contact, :student
+
+  def student_attributes=(attributes)
+    processed_student_attributes = process_name(attributes)
+    super(processed_student_attributes)
+  end
+
+  def contact_attributes=(attributes)
+    processed_contact_attributes = process_name(attributes)
+    super(processed_contact_attributes)
+  end
+
+  private
+
+    def process_name(attributes)
+      full_name = attributes[:full_name]
+      first_name, last_name = first_last_from_full_name(full_name)
+      attributes.merge(first_name: first_name, last_name: last_name)
+    end
+
+    def first_last_from_full_name(full_name)
+      name_parts = full_name.split(' ')
+
+      first = name_parts[0]
+      last = name_parts.length > 1 ? name_parts[-1] : nil
+
+      [ first, last ]
+    end
+
   def self.contact_from_booking_params(booking_params)
     region_id = booking_params[:region_id]
     contact_params = booking_params.select do |param, _|
